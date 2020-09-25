4aacbffa955eaf67629435fb20d4b6d85af138a6
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-09-24 18:58:41 -0600

    Clase 20200924

--- bcksecom/routes/index.js
+++ bcksecom/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get(
+  '/info',
+  function(req, res) {
+    var infoObj = {}; // $infoObj = array();
+    infoObj.cuenta = "0801198412349";
+    infoObj.nombre = "Orlando J Betancourth";
+    infoObj.email = "obetancourthunicah@gmail.com";
+    res.json(infoObj);
+  }
+); // get Info
+
+router.get(
+  '/hook',
+  function(req, res){
+    res.render("hook", {});
+  }
+);// get hook
+
+router.post(
+  '/hook',
+  function(req, res){
+    var body = req.body;
+    console.log(body);
+    res.render("hook", body);
+  }
+  
+);//post hook
+
 module.exports = router;
