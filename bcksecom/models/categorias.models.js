302f9f9280d4b0eed9bbc4d218f4feffa1327e18
Hesler Ferrera <heslerferrera@gmail.com>
2020-10-07 11:15:04 -0600

    creando modelo de datos, getAll, getOne, addOne

---
+++ bcksecom/models/categorias.models.js
@@ -0,0 +1,44 @@
+var db = require('./db')();
+var model = null;
+function initModel(){
+  db.run("CREATE TABLE IF NOT EXISTS categorias(id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, type TEXT)");
+  model = {};
+
+  model.getAll = function (handler) {
+    db.all("SELECT * from categorias;",
+      function (err, rows) {
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, rows);
+        }
+      }
+    )
+  }
+
+  model.getOne = function (id, handler) {
+    db.get("SELECT * from categorias where id = ?;", [id],
+      function (err, row) {
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, row || {});
+        }
+      }
+    )
+  }
+
+  model.addOne = function (category, type, handler) {
+    db.run(
+      "INSERT INTO categorias (category, type) VALUES (?, ?);",
+      [category, type],
+      function (err, rslt) {
+        console.log(rslt);
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, true);
+        }
+      }
+    );
+  }
