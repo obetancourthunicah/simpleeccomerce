d53c2e6e5eb3b88aa1202e59107268cc5b5ee5af
Flopez2998 <60764940+Flopez2998@users.noreply.github.com>
2020-10-07 11:13:00 -0600

    Update categoriasdb.js
    commit  add one, update, delete API.

--- bcksecom/routes/api/categoriasdb.js
+++ bcksecom/routes/api/categoriasdb.js
@@ -25,3 +25,45 @@ router.get('/all', (req, res)=>{
       return res.status(200).json(rslts);
     });
   });
+
+  router.post('/new', (req, res)=>{
+    const {category} = req.body;
+    const {type} = req.body;
+    categoriaModel.addOne(category, type, (err, inserted)=>{
+      if (err) {
+        console.log(err);
+        return res.status(503).json({ "error": "Algo salio mal." });
+      }
+      return res.status(200).json({ inserted });
+    });
+  });
+  
+  router.put('/upd/:id', (req, res)=>{
+    //do something here
+    let {id} = req.params;
+    id = Number(id);
+    let {category} = req.body;
+    let {type} = req.body;
+    categoriaModel.updateOne( id, category, type, (err, updated)=>{
+      if (err) {
+        console.log(err);
+        return res.status(503).json({ "error": "Algo salio mal." });
+      }
+      return res.status(200).json({ updated });
+    });
+  });
+  
+  router.delete('/del/:id', (req, res)=>{
+    let {id} = req.params;
+    id = Number(id);
+    categoriaModel.deleteOne(id, (err, deleted)=>{
+      if (err) {
+        console.log(err);
+        return res.status(503).json({ "error": "Algo salio mal." });
+      }
+      return res.status(200).json({ deleted });
+    });
+  });
+  
+  module.exports = router;
+  
