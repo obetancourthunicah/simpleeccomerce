3a020613920dbe866dd6f19a569af2139970b400
Luis Cerrato <luiscerrato9@gmail.com>
2020-10-08 14:40:55 -0600

    Creando Tabla Categoria

---
+++ bcksecom/models/categorias.model.js
@@ -0,0 +1,95 @@
+var db = require('./db')();
+var model = null;
+function initModel(){
+  db.run("CREATE TABLE IF NOT EXISTS categoria(id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, type TEXT )");
+  model = {};
+
+  /*Get all */
+
+  model.getAll = function (handler) {
+    db.all("SELECT * from categoria;",
+      function (err, rows) {
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, rows);
+        }
+      }
+    )
+  }
+
+  /*Get especifico*/
+
+  model.getOne = function (id, handler) {
+    db.get("SELECT * from categoria where id = ?;", [id],
+      function (err, row) {
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, row || {});
+        }
+      }
+    )
+  }
+
+
+  /* Metodo Post */
+
+  model.addOne = function (category, type, handler) {
+    db.run(
+      "INSERT INTO categoria (category,type) VALUES (?, ?);",
+      [category,type],
+      function (err, rslt) {
+        console.log(rslt);
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, true);
+        }
+      }
+    );
+  }
+
+  /*Metodo Update */
+
+  model.updateOne = function (id, category, type, handler) {
+    db.run(
+      "UPDATE categoria set  category =? , tyoe=? where id = ?;",
+      [ category,type, id],
+      function (err, rslt) {
+        console.log(rslt);
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, true);
+        }
+      }
+    );
+  }
+
+  /*Metodo Delete */
+  model.deleteOne = function (id, handler) {
+    db.run(
+      "DELETE from categoria where id = ?;",
+      [id],
+      function (err, rslt) {
+        console.log(rslt);
+        if (err) {
+          return handler(err, null);
+        } else {
+          return handler(null, true);
+        }
+      }
+    );
+  }
+
+  return model;
+}
+
+module.exports = function () {
+  if (!model) {
+    return initModel();
+  } else {
+    return model;
+  }
+}
